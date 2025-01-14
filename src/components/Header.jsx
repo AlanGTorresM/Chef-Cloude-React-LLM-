86349f9e01ce85724077990141d922240f82acb3
Alan G Torres M <alantorres88@aragon.unam.mx>
2025-01-14 00:14:55 -0600

    intento de action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ const Header = ()=>{
     return(
         <header>
             <div>
-                <i class="fa-solid fa-kitchen-set"></i>
-                <i class="fa solid fa-robot"></i>
+                <i className="fa-solid fa-kitchen-set"></i>
+                <i className="fa solid fa-robot"></i>
             </div>
             <h1>Chef Claude</h1>
         </header>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
 const Main = ()=>{
+
+    // ReactState
+    const [ingredient, setIngredient] = useState([]);
+    const show =ingredient.map(x=><li key={x}>{x}</li>)
+
+    function AddIngredient(e) {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget)
+        const ingredientForm = formData.get('ingredient')
+        setIngredient(prev => [...prev, ingredientForm])
+    }
+    // function AddIngredient(formData){
+    //     const newIngredient = formData.get('ingredient');
+    //     setIngredient(prev => [...prev, newIngredient])
+    // }
+
     return(
-        <form className="add-ingredient-form">
-            <input type="text" placeholder="e.g oregano"></input>
-            <button>+ Add ingridient</button>
-        </form>
+        <>
+            <form /*action={AddIngredient}*/ onSubmit={AddIngredient} className="add-ingredient-form">
+                <input type="text" placeholder="e.g oregano" name="ingredient"></input>
+                <button>+ Add ingridient</button>
+            </form>
+            <ul>
+                {show}
+            </ul>
+        </>
     );
 }
 
